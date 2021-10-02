d1fd5a78f062637a8010d777839444e19e58f902
Edinson Caleno <edinson.caleno.mt@correo.usa.edu.co>
2021-10-02 17:21:55 -0500

    tabla

--- js/funciones.js
+++ js/funciones.js
@@ -11,33 +11,6 @@ function consultarClienteTodo() {
             alert('Petición realizada, ' + xhr.status);
         },
         success: function (json) {
-            <table border="1" cellpadding="3" id="printTable">
-                <tbody><tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Points</th>
-                </tr>
-                    <tr>
-                        <td>Jill</td>
-                        <td>Smith</td>
-                        <td>50</td>
-                    </tr>
-                    <tr>
-                        <td>Eve</td>
-                        <td>Jackson</td>
-                        <td>94</td>
-                    </tr>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>80</td>
-                    </tr>
-                    <tr>
-                        <td>Adam</td>
-                        <td>Johnson</td>
-                        <td>67</td>
-                    </tr>
-                </tbody></table>
             $("#resultado").empty();
             for (i = 0; i < json.items.length; i++) {
 
