e1764f33cf0bb0ec433b65565da3d5bae57e4ffc
Edinson Caleno <edinson.caleno.mt@correo.usa.edu.co>
2021-10-02 15:29:50 -0500

    COMILLA

--- js/funciones.js
+++ js/funciones.js
@@ -48,7 +48,7 @@ function editarCliente() {
     $.ajax({
         url: 'https://g54ed9b48eae3a2-edinsondb.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         data: {
-            id: $('#ide').val(),
+            id: $("#ide").val(),
             name: $("#nombre").val(),
             email: $("#correo").val(),
             age: $('#edad').val()
@@ -72,7 +72,7 @@ function eliminarCliente() {
     $.ajax({
         url: 'https://g54ed9b48eae3a2-edinsondb.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         data: {
-            id: $('#ide').val()
+            id: $("#ide").val()
         },
         type: 'DELETE',
         dataType: 'json',
