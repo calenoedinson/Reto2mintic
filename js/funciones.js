878d42bc9eae90d020f8b9ba3c6077aee9010e93
Edinson Caleno <edinson.caleno.mt@correo.usa.edu.co>
2021-10-02 15:27:58 -0500

    CAMBIO

--- js/funciones.js
+++ js/funciones.js
@@ -72,7 +72,7 @@ function eliminarCliente() {
     $.ajax({
         url: 'https://g54ed9b48eae3a2-edinsondb.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         data: {
-            id:  $('#ide').val()
+            id: $('#ide').val()
         },
         type: 'DELETE',
         dataType: 'json',
